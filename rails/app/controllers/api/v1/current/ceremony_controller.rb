9cf6b4729f26f1cd0fa36f027459473325c8bb63
mametaro99 <bearsmichimato99@gmail.com>
2024-11-28 12:09:23 +0900

    式典の新規作成メソッドを実装

--- rails/app/controllers/api/v1/current/ceremony_controller.rb
+++ rails/app/controllers/api/v1/current/ceremony_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Current::CeremonyController < Api::V1::BaseController
-  before_action :authenticate_user!, only: [:index, :create, :update, :destroy]
-  before_action :set_ceremony, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :create, :destroy]
+  before_action :set_ceremony, only: [:show, :destroy]
 
   # GET /api/v1/current/ceremonies
   def index
@@ -15,23 +15,21 @@ class Api::V1::Current::CeremonyController < Api::V1::BaseController
 
   # POST /api/v1/current/ceremonies
   def create
-    @ceremony = Ceremony.new(ceremony_params)
-    @ceremony.user = current_user
+    ActiveRecord::Base.transaction do
+      @ceremony = Ceremony.new(ceremony_params.except(:ceremony_okyo_groups_attributes))
+      @ceremony.user = current_user
 
-    if @ceremony.save
-      render json: @ceremony, status: :created
-    else
-      render json: { errors: @ceremony.errors.full_messages }, status: :unprocessable_entity
+      if @ceremony.save
+        # CeremonyOkyoGroupsを作成
+        handle_ceremony_okyo_groups(@ceremony)
+        render json: @ceremony, status: :created
+      else
+        render json: { errors: @ceremony.errors.full_messages }, status: :unprocessable_entity
+      end
     end
-  end
-
-  # PATCH/PUT /api/v1/current/ceremonies/:id
-  def update
-    if @ceremony.update(ceremony_params)
-      render json: @ceremony, status: :ok
-    else
-      render json: { errors: @ceremony.errors.full_messages }, status: :unprocessable_entity
-    end
+  rescue => e
+    Rails.logger.error("Ceremony creation failed: #{e.message}")
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 
   # DELETE /api/v1/current/ceremonies/:id
@@ -41,6 +39,29 @@ class Api::V1::Current::CeremonyController < Api::V1::BaseController
   end
 
   private
+
+  # CeremonyOkyoGroupの新規作成と更新を処理
+  def handle_ceremony_okyo_groups(ceremony)
+    attributes = ceremony_params[:ceremony_okyo_groups_attributes]
+    return if attributes.blank?
+
+    Rails.logger.info("attributes: #{attributes}")
+
+    attributes.each do |group_params|
+      if group_params[:id].to_i.positive?
+        # 正当なIDの場合は既存レコードの更新を試みる
+        group = CeremonyOkyoGroup.find_by(id: group_params[:id])
+        if group
+          group.update!(group_params.except(:id))
+        else
+          raise ActiveRecord::RecordNotFound, "Couldn't find CeremonyOkyoGroup with ID=#{group_params[:id]}"
+        end
+      else
+        # IDがnilまたは負の値の場合は新規作成
+        ceremony.ceremony_okyo_groups.create!(group_params.except(:id))
+      end
+    end
+  end
 
   # Sets @ceremony based on the provided id
   def set_ceremony
@@ -52,7 +73,7 @@ class Api::V1::Current::CeremonyController < Api::V1::BaseController
   def ceremony_params
     params.require(:ceremony).permit(
       :name, :event_date, :location, :description,
-      ceremony_okyo_groups_attributes: [:id, :okyo_id, :order, :_destroy]
+      ceremony_okyo_groups_attributes: [:id, :okyo_id, :order]
     )
-  end  
+  end
 end
