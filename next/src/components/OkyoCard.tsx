6874548bfe390e6677f284f17944e65a6366697a
mametaro99 <bearsmichimato99@gmail.com>
2024-11-29 15:01:46 +0900

    お経の一覧ページの修正

---
+++ next/src/components/OkyoCard.tsx
@@ -0,0 +1,35 @@
+import { Box, Card, CardContent, Typography, Link } from '@mui/material'
+
+type OkyoCardProps = {
+  name: string
+  description: string
+}
+
+const omit = (text: string) => (len: number) => (ellipsis: string) =>
+  text.length >= len ? text.slice(0, len - ellipsis.length) + ellipsis : text
+
+const OkyoCard = (props: OkyoCardProps) => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography
+          component="h3"
+          sx={{
+            mb: 2,
+            minHeight: 48,
+            fontSize: 16,
+            fontWeight: 'bold',
+            lineHeight: 1.5,
+          }}
+        >
+          {omit(props.name)(45)('...')}
+        </Typography>
+        <Typography sx={{ mb: 2, fontSize: 14, color: 'text.secondary' }}>
+          {omit(props.description)(60)('...')}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default OkyoCard
--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,19 +1,46 @@
+import { Box, Grid, Container, Link } from '@mui/material'
+import camelcaseKeys from 'camelcase-keys'
 import type { NextPage } from 'next'
 import useSWR from 'swr'
+import OkyoCard from '@/components/OkyoCard'
 import { fetcher } from '@/utils'
 
-const Index: NextPage = () => {
-  const url = 'http://localhost:3000/api/v1/health_check'
-  const { data, error } = useSWR(url, fetcher)
+type OkyoProps = {
+  id: number
+  name: string
+  description: string
+  videoUrl: string
+  articleUrl: string
+  createdAt: string
+}
 
+const Index: NextPage = () => {
+  const url = 'http://localhost:3000/api/v1/okyo'
+
+  const { data, error } = useSWR(url, fetcher)
   if (error) return <div>An error has occurred.</div>
   if (!data) return <div>Loading...</div>
 
+  const okyos = camelcaseKeys(data)
+  console.log(okyos)
+
   return (
-    <>
-      <div>Rails疎通確認</div>
-      <div>レスポンスメッセージ: {data.message}</div>
-    </>
+    <Box sx={{ backgroundColor: '#e6f2ff', minHeight: '100vh' }}>
+      <Container maxWidth="md" sx={{ pt: 6 }}>
+        <Grid container spacing={4}>
+          {okyos.map((okyo: OkyoProps, i: number) => (
+            <Grid key={okyo.id} item xs={12} md={6}>
+              <Link href={'/articles/' + okyo.id}>
+                <OkyoCard
+                  name={okyo.name}
+                  description={okyo.description}
+                />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
   )
 }
 
