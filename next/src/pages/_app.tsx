8eaf0fd9322dfd828bee5175f698b77990604635
mametaro99 <bearsmichimato99@gmail.com>
2024-12-01 21:57:21 +0900

    ログイン・ログアウト機能の実装

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -4,9 +4,10 @@ import { ThemeProvider } from '@mui/material/styles'
 import { AppProps } from 'next/app'
 import '@/styles/destyle.css'
 import Header from '@/components/Header'
-
+import CurrentUserFetch from '@/components/CurrentUserFetch'
 import createEmotionCache from '@/styles/createEmotionCache'
 import theme from '@/styles/theme'
+import Snackbar from '@/components/Snackbar'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -22,8 +23,10 @@ export default function MyApp(props: MyAppProps): JSX.Element {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <CurrentUserFetch />
         <Header />
         <Component {...pageProps} />
+        <Snackbar />
       </ThemeProvider>
     </CacheProvider>
   )
