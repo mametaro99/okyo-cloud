e64dd7fe0c8dce8d6c2e677fcbd4044d67007f48
mametaro99 <bearsmichimato99@gmail.com>
2024-12-05 18:15:52 +0900

    Userの詳細情報に宗派を付与

--- rails/app/serializers/current_user_serializer.rb
+++ rails/app/serializers/current_user_serializer.rb
@@ -1,3 +1,4 @@
 class CurrentUserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :location, :sect, :created_at, :updated_at
+  belongs_to :sect, serializer: SectSerializer
 end
