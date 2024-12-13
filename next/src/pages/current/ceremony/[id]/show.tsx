bc7fa6f58cf1f6cc630da1643d852129651eaca6
mametaro99 <bearsmichimato99@gmail.com>
2024-12-13 16:09:30 +0900

    式典ページの詳細画面の背景を変更、またQRコードを説明を追加

--- next/src/pages/current/ceremony/[id]/show.tsx
+++ next/src/pages/current/ceremony/[id]/show.tsx
@@ -15,6 +15,7 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Box,
 } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useRouter } from "next/router";
@@ -25,6 +26,7 @@ import Error from "@/components/Error";
 import camelcaseKeys from "camelcase-keys";
 import { QRCodeCanvas as QRCode } from "qrcode.react";
 import { useUserState } from "@/hooks/useGlobalState";
+import { styles } from "@/styles";
 
 interface OkyoPhrase {
   id: string;
@@ -81,89 +83,104 @@ const CeremonyDetail: NextPage = () => {
   const isUserCreator = user.isSignedIn && user.id === ceremonyData.userId; // ユーザーが作成者か確認
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        {ceremonyData.name}
-      </Typography>
+    <Box
+      css={styles.pageMinHeight}
+      sx={{
+        background: 'url(/unkai.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'repeat',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          {ceremonyData.name}
+        </Typography>
 
-      {isUserCreator && (
-        <div>
-          <Typography variant="h6" gutterBottom>
-            QRコード:
-          </Typography>
-          <QRCode value={`${url}/${id}/show`} />
-        </div>
-      )}
-      <Typography variant="body1" gutterBottom>
-        {ceremonyData.description}
-      </Typography>
-
-      {ceremonyData.ceremonyOkyoGroups.map((group) => (
-        <div key={group.id}>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls={`panel-${group.id}-content`}
-              id={`panel-${group.id}-header`}
-            >
-              <Typography variant="h6" gutterBottom>
-                {group.okyo.name}
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <List>
-                <ListItem>
-                  <ListItemText
-                  secondary={
-                    <>
-                    <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-                      {group.okyo.okyoPhrases.map((phrase, index) => (
-                      <div key={phrase.id}>
-                        {phrase.phraseText}
-                        {phrase.reading && (
-                        <Typography variant="body2" color="text.secondary">
-                          {phrase.reading}
-                        </Typography>
-                        )}
-                      </div>
-                      ))}
-                    </Typography>
-                    </>
-                  }
-                 />
-                </ListItem>
-              </List>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-      ))}
-
-      <Dialog open={dialogOpen} onClose={handleDialogClose}>
-        {selectedOkyo && (
-          <>
-            <DialogTitle>{selectedOkyo.name}</DialogTitle>
-            <DialogContent>
-              <DialogContentText>{selectedOkyo.description}</DialogContentText>
-              <Typography variant="h6" gutterBottom>
-                Phrases:
-              </Typography>
-              <List>
-                {selectedOkyo.phrases.map((phrase) => (
-                  <ListItem key={phrase.id}>
-                    <ListItemText primary={phrase.text} />
-                  </ListItem>
-                ))}
-              </List>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleDialogClose} color="primary">
-                Close
-              </Button>
-            </DialogActions>
-          </>
+        {isUserCreator && (
+          <div>
+            <Typography variant="h6" gutterBottom>
+              こちらのQRコードを読み取ると、本日のお経の一覧をみることができます。
+            </Typography>
+            <QRCode value={`${url}/${id}/show`} />
+          </div>
         )}
-      </Dialog>
-    </Container>
+        <Typography variant="body1" gutterBottom>
+          {ceremonyData.description}
+        </Typography>
+
+        {ceremonyData.ceremonyOkyoGroups.map((group) => (
+          <div key={group.id}>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={`panel-${group.id}-content`}
+                id={`panel-${group.id}-header`}
+              >
+                <Typography variant="h6" gutterBottom>
+                  {group.okyo.name}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List>
+                  <ListItem>
+                    <ListItemText
+                    secondary={
+                      <>
+                      <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+                        {group.okyo.okyoPhrases.map((phrase, index) => (
+                        <div key={phrase.id}>
+                          {phrase.phraseText}
+                          {phrase.reading && (
+                          <Typography variant="body2" color="text.secondary">
+                            {phrase.reading}
+                          </Typography>
+                          )}
+                        </div>
+                        ))}
+                      </Typography>
+                      </>
+                    }
+                  />
+                  </ListItem>
+                </List>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        ))}
+
+        <Dialog open={dialogOpen} onClose={handleDialogClose}>
+          {selectedOkyo && (
+            <>
+              <DialogTitle>{selectedOkyo.name}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>{selectedOkyo.description}</DialogContentText>
+                <Typography variant="h6" gutterBottom>
+                  Phrases:
+                </Typography>
+                <List>
+                  {selectedOkyo.phrases.map((phrase) => (
+                    <ListItem key={phrase.id}>
+                      <ListItemText primary={phrase.text} />
+                    </ListItem>
+                  ))}
+                </List>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleDialogClose} color="primary">
+                  Close
+                </Button>
+              </DialogActions>
+            </>
+          )}
+        </Dialog>
+      </Container>
+    </Box>
   );
 };
 
