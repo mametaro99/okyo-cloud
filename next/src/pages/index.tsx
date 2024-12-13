45bf3b373d1671cd454e1f17d8c4d3dc5a7a0e3d
mametaro99 <bearsmichimato99@gmail.com>
2024-12-13 16:07:29 +0900

    ホーム画面の背景、お経のブロックの大きさを調整

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -29,11 +29,29 @@ const Index: NextPage = () => {
   console.log(okyos)
 
   return (
-    <Box css={styles.pageMinHeight} sx={{ backgroundColor: '#e6f2ff' }}>
+    <Box
+      css={styles.pageMinHeight}
+      sx={{
+        backgroundImage: 'url(/unkai.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        animation: 'fadeIn 1.5s ease-in-out',
+        '@keyframes fadeIn': {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        }
+      }}
+    >
       <Container maxWidth="md" sx={{ pt: 6 }}>
         <Grid container spacing={4}>
           {okyos.map((okyo: OkyoProps, i: number) => (
-            <Grid key={okyo.id} item xs={12} md={6}>
+            <Grid key={okyo.id} item xs={6} md={4}>
               <Link href={'/okyos/' + okyo.id}>
                 <OkyoCard
                   name={okyo.name}
@@ -43,6 +61,31 @@ const Index: NextPage = () => {
             </Grid>
           ))}
         </Grid>
+      </Container>
+      <Container
+        maxWidth="md"
+        sx={{
+          pt: 6,
+          animation: 'floatUp 2s ease forwards',
+          opacity: 0,
+          '@keyframes floatUp': {
+            from: { transform: 'translateY(20px)', opacity: 0 },
+            to: { transform: 'translateY(0)', opacity: 1 },
+          },
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: 'rgba(255, 255, 255, 0.8)', // 半透明の白背景
+            padding: 3,
+            borderRadius: 2,
+            boxShadow: 3,
+            textAlign: 'center',
+          }}
+        >
+          このアプリはお経の動画・音声に合わせて、お経の意味や読み方を表示するアプリです。初学者や仏道を目指す方にとってより分かりやすくお経を理解するために作りました。
+          また、葬式などの式典で配られる経本（お経の書かれた冊子）について、アプリで代用することができます。
+        </Box>
       </Container>
     </Box>
   )
