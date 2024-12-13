e553821652ea06c42a437dab116b4a724612358a
mametaro99 <bearsmichimato99@gmail.com>
2024-12-13 16:08:45 +0900

    式典ページの背景を変更

--- next/src/pages/current/ceremony/index.tsx
+++ next/src/pages/current/ceremony/index.tsx
@@ -16,6 +16,7 @@ import Loading from "@/components/Loading";
 import Error from "@/components/Error";
 import camelcaseKeys from "camelcase-keys";
 import { useRequireSignedIn } from "@/hooks/useRequireSignedIn";
+import { styles } from "@/styles";
 
 interface Ceremony {
   id: string;
@@ -40,69 +41,102 @@ const CeremonyList: NextPage = () => {
   const ceremonyList = ceremonies.map((ceremony) => camelcaseKeys({ ...ceremony }));
 
   return (
-    <Container sx={{ maxWidth: "800px", mx: "auto", p: 2 }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        式典・葬式・葬式一覧
-      </Typography>
+    <Box
+      css={styles.pageMinHeight}
+      sx={{
+        background: 'url(/unkai.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'repeat',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Container sx={{ maxWidth: "800px", mx: "auto", p: 2, backgroundColor: 'rgba(255, 255, 255, 0.4)' }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          式典・葬式一覧
+        </Typography>
 
-      <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{
-            color: 'white',
-            textTransform: 'none',
-            fontSize: 16,
-            borderRadius: 2,
-            width: 100,
-            boxShadow: 'none',
-          }}
-          onClick={() => router.push("/current/ceremony/new")}
-        >
-          新規作成
-        </Button>
-      </Box>
-
-      <List>
-        {ceremonyList.map((ceremony) => (
-          <ListItem
-            key={ceremony.id}
+        <Box sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}>
+          <Button
+            variant="contained"
+            color="primary"
             sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              border: "1px solid #ddd",
-              borderRadius: "8px",
-              mb: 2,
-              p: 2,
+              color: 'white',
+              textTransform: 'none',
+              fontSize: 16,
+              borderRadius: 2,
+              width: 100,
+              boxShadow: 'none',
             }}
+            onClick={() => router.push("/current/ceremony/new")}
           >
-            <ListItemText
-              primary={ceremony.name}
-              secondary={ceremony.location}
-            />
+            新規作成
+          </Button>
+        </Box>
 
-            <Box sx={{ display: "flex", gap: 2 }}>
-              <Button
-                variant="outlined"
-                color="info"
-                onClick={() => router.push(`/current/ceremony/${ceremony.id}/show`)}
-              >
-                詳細
-              </Button>
+        <List>
+          {ceremonyList.map((ceremony) => (
+            <ListItem
+              key={ceremony.id}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                border: "1px solid #ddd",
+                borderRadius: "8px",
+                mb: 2,
+                p: 2,
+              }}
+            >
+              <ListItemText
+                primary={ceremony.name}
+                secondary={ceremony.location}
+                // もう少し文字を濃ゆく表示
+                primaryTypographyProps={{ variant: "h6" }}
+              />
 
-              <Button
-                variant="outlined"
-                color="secondary"
-                onClick={() => router.push(`/current/ceremony/${ceremony.id}/edit`)}
-              >
-                編集
-              </Button>
-            </Box>
-          </ListItem>
-        ))}
-      </List>
-    </Container>
+              <Box sx={{ display: "flex", gap: 2 }}>
+                <Button
+                  variant="contained"
+                  color="info"
+                  sx={{
+                    color: 'white',
+                    textTransform: 'none',
+                    fontSize: 16,
+                    borderRadius: 2,
+                    width: 100,
+                    boxShadow: 'none',
+                  }}
+                  onClick={() => router.push(`/current/ceremony/${ceremony.id}/show`)}
+                >
+                  詳細
+                </Button>
+
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  sx={{
+                    color: 'white',
+                    textTransform: 'none',
+                    fontSize: 16,
+                    borderRadius: 2,
+                    width: 100,
+                    boxShadow: 'none',
+                  }}
+                  onClick={() => router.push(`/current/ceremony/${ceremony.id}/edit`)}
+                >
+                  編集
+                </Button>
+              </Box>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
+    </Box>
   );
 };
 
