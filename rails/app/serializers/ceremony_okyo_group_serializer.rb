9859471f3accfe861acbdb23abdd3bffa7dc631d
mametaro99 <bearsmichimato99@gmail.com>
2024-12-13 12:34:56 +0900

    idを含めるように変更

--- rails/app/serializers/ceremony_okyo_group_serializer.rb
+++ rails/app/serializers/ceremony_okyo_group_serializer.rb
@@ -1,4 +1,4 @@
 class CeremonyOkyoGroupSerializer < ActiveModel::Serializer
-  attributes :order, :okyo_id, :ceremony_id, :order, :okyo
+  attributes :id, :order, :okyo_id, :ceremony_id, :order, :okyo
   belongs_to :okyo, serializer: OkyoSerializer
 end
