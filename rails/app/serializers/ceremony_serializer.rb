f86b3735f3ae9c767520e88f978fbaea3a268a34
mametaro99 <bearsmichimato99@gmail.com>
2024-12-13 12:29:58 +0900

    式典の詳細情報でlocationとevent_dateを返すように変更

--- rails/app/serializers/ceremony_serializer.rb
+++ rails/app/serializers/ceremony_serializer.rb
@@ -1,4 +1,4 @@
 class CeremonySerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :user_id, :created_at, :updated_at
+  attributes :id, :name, :description, :location, :event_date, :user_id, :created_at, :updated_at
   has_many :ceremony_okyo_groups, each_serializer: CeremonyOkyoGroupSerializer
 end
