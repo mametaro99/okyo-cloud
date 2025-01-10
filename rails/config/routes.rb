254e545f6ae8d1015d37884f301ef1f3d23c132b
mametaro99 <bearsmichimato99@gmail.com>
2025-01-10 14:43:16 +0900

    ユーザの登録のリンクを作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -22,6 +22,10 @@ Rails.application.routes.draw do
         get "user", to: "users#show"
         resources :user, only: [:show]
       end
+
+      namespace :user do
+        resource :confirmations, only: [:update]
+      end
     end
   end
 end
