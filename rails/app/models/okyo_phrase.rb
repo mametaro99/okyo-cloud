7cd40edda96249aca5d0db504a447b834edd4d32
mametaro99 <bearsmichimato99@gmail.com>
2025-01-31 13:51:07 +0900

    お経のフレーズのバリデーションを追加

--- rails/app/models/okyo_phrase.rb
+++ rails/app/models/okyo_phrase.rb
@@ -1,3 +1,6 @@
 class OkyoPhrase < ApplicationRecord
   belongs_to :okyo
+  validation :phrase_text, presence: true
+  validation :meaning, presence: true
+  validation :reading, presence: true
 end
