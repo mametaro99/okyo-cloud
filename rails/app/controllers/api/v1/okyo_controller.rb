f36a430a39084b5d35632218ab88d03e8f1eee61
mametaro99 <bearsmichimato99@gmail.com>
2025-02-03 20:33:02 +0900

    公開されているお経の情報をRedisでキャッシュする

--- rails/app/controllers/api/v1/okyo_controller.rb
+++ rails/app/controllers/api/v1/okyo_controller.rb
@@ -1,12 +1,19 @@
 class Api::V1::OkyoController < ApplicationController
   def index
-    @okyos = Okyo.includes(:sects).includes(:okyo_phrases)
-    @published_okyos = @okyos.select { |okyo| okyo.published }
+    @published_okyos = cache_published_okyos
     render json: @published_okyos, each_serializer: OkyoSerializer, status: :ok
   end
 
   def show
     @okyo = Okyo.find(params[:id])
-    render json: @okyo, serializer: OkyoSerializer, status: :ok
+    render json: @okyo, serializer: ShowOkyoSerializer, status: :ok
   end
+
+  private
+
+    def cache_published_okyos
+      Rails.cache.fetch("okyos", expires_in: 1.hour) do
+        Okyo.includes(:sects).select { |okyo| okyo.published }.to_a
+      end
+    end
 end
